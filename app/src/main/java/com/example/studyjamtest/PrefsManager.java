25abb53c22b0008168ede0b14e101f7927fe7215
snigavig <snigavig@gmail.com>
2015-01-22 18:35:00 +0200

    cleanup

--- app/src/main/java/com/example/studyjamtest/PrefsManager.java
+++ app/src/main/java/com/example/studyjamtest/PrefsManager.java
@@ -8,10 +8,11 @@ import android.preference.PreferenceManager;
  * Created by snigavig on 22.01.15.
  */
 public class PrefsManager {
-    private static final String IS_LOGIN_KEY = "IS_RUNNING";
+    private static final String IS_LOGIN_KEY = "IS_LOGIN";
     private static final String LOGIN_KEY = "LOGIN";
     private static final String PASSWORD_KEY = "PASSWORD";
     private SharedPreferences prefs;
+
     public PrefsManager(Context context) {
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
